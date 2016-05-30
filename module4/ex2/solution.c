f4f4eca615073b51fafd4e5685db86cf3e2ffc50
Mukhad <mukhad@yandex.ru>
2016-05-30 11:38:08 +0000

    mod4 ex2 v02

--- module4/ex2/solution.c
+++ module4/ex2/solution.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+#include <dirent.h>
 
 int main() {
    char key[] = "PPid:";
    FILE *fp;
    char buff[255];
+
+DIR *pDIR;
+        struct dirent *entry;
+        if( pDIR=opendir("/proc") ){
+            while(entry = readdir(pDIR)){
+                if( strcmp(entry->d_name, ".") != 0 && 
+                    strcmp(entry->d_name, "..") != 0 &&
+                    isdigit(entry->d_name[0]) )
+                        printf("%s    %d\n", entry->d_name,entry->d_type );
+            }
+            closedir(pDIR);
+        }
 
    fp = fopen("/proc/self/status", "r");
    int i=0;
