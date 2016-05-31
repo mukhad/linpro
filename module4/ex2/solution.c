286a23139edbaf227b846c9cf7ca40af95ad95ca
Mukhad <mukhad@yandex.ru>
2016-05-31 08:56:06 +0000

    mod4 ex2 v03

--- module4/ex2/solution.c
+++ module4/ex2/solution.c
@@ -7,31 +7,39 @@ int main() {
    FILE *fp;
    char buff[255];
 
-DIR *pDIR;
-        struct dirent *entry;
-        if( pDIR=opendir("/proc") ){
-            while(entry = readdir(pDIR)){
+   DIR *pDIR;
+   struct dirent *entry;
+   int cnt=0;
+
+   if( pDIR=opendir("/proc") ){
+        while(entry = readdir(pDIR)){
                 if( strcmp(entry->d_name, ".") != 0 && 
                     strcmp(entry->d_name, "..") != 0 &&
-                    isdigit(entry->d_name[0]) )
-                        printf("%s    %d\n", entry->d_name,entry->d_type );
-            }
-            closedir(pDIR);
-        }
-
-   fp = fopen("/proc/self/status", "r");
-   int i=0;
-   do{
+                    isdigit(entry->d_name[0]) ){
+   			char fname[255]={0};	
+			strcat(fname,"/proc/");
+			strcat(fname,entry->d_name);     
+			strcat(fname,"/status");
+			
+   		fp = fopen(fname, "r");
+		int i = 1;
+		if(fp == NULL) i = 0;
    
-      fscanf(fp, "%s", buff);
-      i = strcmp(key,buff);	
-      //printf("$%s$   %d\n", buff,i);
-   }while(i!=0);
+     		fscanf(fp, "%s", buff);
+     		fscanf(fp, "%s", buff);
 
-      fscanf(fp, "%s", buff);
-      printf("%s\n", buff);
-   fclose(fp);
-   
+                int c = strcmp("genenv",buff);	
+		if(c==0) cnt++;
+ 		 
+		printf("%s   %d   %s   %s\n ",entry->d_name,i,fname,buff);
+
+
+		fclose(fp);
+        	}     
+	}
+        closedir(pDIR);
+   }
+   printf("%d\n",cnt);
 
    return 0;
 }
